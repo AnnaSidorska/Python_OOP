5462e90907aaeb7071a3d1a10be21c160a793963
AnnaSidorska <annasidorska13@gmail.com>
2021-09-15 22:32:47 +0300

    Update Task_2.py

--- Lab_1/Task_2.py
+++ Lab_1/Task_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 
 dictionaryOfOperators = {
@@ -10,16 +9,3 @@ dictionaryOfOperators = {
 operator = dictionaryOfOperators.get(sys.argv[1])
 print(eval(sys.argv[2] + operator + sys.argv[3]))
 
-=======
-import sys
-
-dictionaryOfOperators = {
-    'add': '+',
-    'minus': '-',
-    'multiply': '*',
-    'divide': '/'
-}
-operator = dictionaryOfOperators.get(sys.argv[1])
-print(eval(sys.argv[2] + operator + sys.argv[3]))
-
->>>>>>> 3323380d7fef3aaa5571d1013bb3baacc558667d
