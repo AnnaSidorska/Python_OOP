0c0883fbeda444e82e5886f90c47741c21b601eb
AnnaSidorska <annasidorska13@gmail.com>
2021-09-16 12:11:01 +0300

    Update Task_3.py

--- Lab_1/Task_3.py
+++ Lab_1/Task_3.py
@@ -1,5 +1,26 @@
 import sys
-digits = '0123456789'
 signs = '+-'
-indexFormula = True;
 
+def checkFormula(userInput):
+    indicator = True
+    for userFormula in userInput:
+        if userFormula.isdigit():
+            indicator = True
+        elif indicator and userFormula in signs:
+            indicator = False
+        elif indicator == True and (userFormula == '(' or ')'):
+            indicator = True
+        else:
+            return 0
+    if indicator:
+        return 1
+    return 0
+
+if __name__ == "__main__":
+    result = ''
+    for index in range(1, len(sys.argv)):
+        result += sys.argv[index]
+    if checkFormula(result):
+        print('(True, {})'.format(eval(result)))
+    else:
+        print('(False, None)')
