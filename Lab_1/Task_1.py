71a240fe4e82c5d8cd697b150374c66888e71ac9
AnnaSidorska <annasidorska13@gmail.com>
2021-09-16 12:13:15 +0300

    Update Task_1.py

--- Lab_1/Task_1.py
+++ Lab_1/Task_1.py
@@ -1,7 +1,2 @@
-# Write a Python-script that performs simple arithmetic operations: '+', '-', '*', '/'.
-# The type of operator and data are set on the command line when the script is run.
-# The script should be launched like this:
-# >>python my_task.py 1 * 2
-
 import sys
 print(eval(sys.argv[1] + sys.argv[2] + sys.argv[3]))
