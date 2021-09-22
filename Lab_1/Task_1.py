d7326193941e560616493f60b25d0a5d2bacac0e
AnnaSidorska <annasidorska13@gmail.com>
2021-09-22 23:38:42 +0300

    Update Task_1.py

--- Lab_1/Task_1.py
+++ Lab_1/Task_1.py
@@ -1,2 +1,10 @@
 import sys
-print(eval(''.join(sys.argv[1::])))
+
+operators = ['/', '*', '-', '+']
+if len(sys.argv) == 4 and sys.argv[1].isdigit() and sys.argv[2] in operators and sys.argv[3].isdigit():
+    try:
+        print(eval(sys.argv[1] + sys.argv[2] + sys.argv[3]))
+    except ZeroDivisionError:
+        print("Division on zero is impossible!")
+else:
+    print("Wrong input! Try again.")
