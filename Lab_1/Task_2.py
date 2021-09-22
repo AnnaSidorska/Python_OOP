66824c38db236a20ad86db889b11f97d587bcb77
AnnaSidorska <annasidorska13@gmail.com>
2021-09-22 23:42:58 +0300

    Update Task_2.py

--- Lab_1/Task_2.py
+++ Lab_1/Task_2.py
@@ -7,5 +7,12 @@ dictionaryOfOperators = {
     'divide': '/'
 }
 operator = dictionaryOfOperators.get(sys.argv[1])
-print(eval(sys.argv[2] + operator + sys.argv[3]))
+if len(sys.argv) == 4 and sys.argv[2].isdigit() and sys.argv[3].isdigit():
+    try:
+        print(eval(sys.argv[2] + operator + sys.argv[3]))
+    except ZeroDivisionError:
+        print("Division on zero is impossible!")
+else:
+    print("Wrong input! Try again.")
+
 
