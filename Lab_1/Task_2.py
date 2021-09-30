c7a55e07ca4ae6d593978d8d0a426052f67c9965
AnnaSidorska <annasidorska13@gmail.com>
2021-09-30 23:39:59 +0300

    Update Task_2.py

--- Lab_1/Task_2.py
+++ Lab_1/Task_2.py
@@ -6,10 +6,18 @@ dictionaryOfOperators = {
     'multiply': '*',
     'divide': '/'
 }
+""" Makes a dictionary of the operators. """
+
 operator = dictionaryOfOperators.get(sys.argv[1])
+""" Get the right operator in the dictionary. """
+
 if len(sys.argv) == 4 and sys.argv[2].isdigit() and sys.argv[3].isdigit():
+    """ Checks if the input is right and have 4 arguments. """
+    
     try:
         print(eval(sys.argv[2] + operator + sys.argv[3]))
+        """ Prints the result. """
+        
     except ZeroDivisionError:
         print("Division on zero is impossible!")
 else:
