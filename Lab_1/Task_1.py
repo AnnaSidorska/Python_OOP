885cf5bab44f3fb10015ffb11bbd72e7bac53b3a
AnnaSidorska <annasidorska13@gmail.com>
2021-09-30 23:40:02 +0300

    Update Task_1.py

--- Lab_1/Task_1.py
+++ Lab_1/Task_1.py
@@ -1,9 +1,15 @@
 import sys
 
 operators = ['/', '*', '-', '+']
+""" Makes a list of the operators. """
+
 if len(sys.argv) == 4 and sys.argv[1].isdigit() and sys.argv[2] in operators and sys.argv[3].isdigit():
+    """ Checks if the input is right and have 4 arguments. """
+    
     try:
         print(eval(sys.argv[1] + sys.argv[2] + sys.argv[3]))
+        """ Prints the result. """
+        
     except ZeroDivisionError:
         print("Division on zero is impossible!")
 else:
